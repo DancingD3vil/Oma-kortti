2e3268a885f8a7dfd1d0613f6a8a667dfa8ada52
unknown <martikainentatu@gmail.com>
2024-01-10 13:31:30 +0200

    added files

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import contacts from './contacts.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <View style={styles.title}>
+          <Text>ButtButt</Text>
+        </View>
+        <View style={styles.main}>
+          <Text>{JSON.stringify(contacts)}</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 0.8,
+    backgroundColor: '#fff',
+  },
+  title: {
+    flex: 0.2,
+    backgroundColor: '#fff',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: '#bbb',
     alignItems: 'center',
     justifyContent: 'center',
   },

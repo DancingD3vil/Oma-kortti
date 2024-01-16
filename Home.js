877803e3b5582009adbcc016b990f97130ff61ea
slketola <saraliisaketola@gmail.com>
2024-01-16 13:37:07 +0200

    added dysfunctional function to home.js

--- Home.js
+++ Home.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import contacts from './contacts.json';
+
 
 export default function Home() {
-  return (
-    <View><Text>ababa</Text></View>
-  );
+    getStarredContacts()
+}
+
+function getStarredContacts() {
+  for (i=0; i<contacts.contacts.length; i++){
+    var contact = createContactComponent(i);
+    return contact
+  }
+}
+function createContactComponent(index){
+  return (<Text>{index}</Text>)
 }

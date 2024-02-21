f602c1677129211b070b2be1da911c5f6de01361
unknown <saraliisaketola@gmail.com>
2024-02-21 10:40:21 +0200

    fixing home css

--- Home.js
+++ Home.js
@@ -5,8 +5,6 @@ import UserAvatar from 'react-native-user-avatar';
 import { useState } from 'react';
 import contactFile from './components/Contact.js'
 
-
-
 export default function Home({ contactInfo }) {
   const [contactArray, updateContactArray] = useState([])
   const [starredContactList, updateStarredContactList] = useState(contactInfo.starredContacts.starredContacts)
@@ -14,9 +12,19 @@ export default function Home({ contactInfo }) {
   var targetList = contactInfo.contacts.filter((id) => contactInfo.starredContacts.starredContacts.includes(id.id))
   if (targetList.length > 0) {
     for (let i = 0; i < targetList.length; i++) {
+      var targetUnit = targetList[i].unit.toLowerCase()
+      var unitStyle
+      alert(targetUnit)
+      switch (targetUnit) {
+        case 'taitamo':
+          unitStyle = "#FFFFFF";
+        case 'luotsi':
+          unitStyle = "#FGFGFG";
+        
+      }
       //use index to push correct contact
       contactArray.push(
-        <View style={styles.cardBox}>
+        <View style={styles.cardBox} perse={backgroundColor={unitStyle}}>
           <UserAvatar size={45} style={styles.avatar} src={targetList[i].avatar} name={targetList[i].firstName + ' ' + targetList[i].lastName} />
           <Text style={styles.text}>{targetList[i].firstName} {targetList[i].lastName}</Text>
           <Text style={styles.text}>{targetList[i].unit}</Text>
@@ -26,34 +34,31 @@ export default function Home({ contactInfo }) {
     }
   }
   else {
-    contactArray.push(
-      <View><Text>Looks like there's nothing here, wanna add something?</Text>
-      </View>
-    )
+    contactArray.push(<View><Text>Looks like there's nothing here, head to the contact menu to add your contact person!</Text></View>)
   }
-  return <SafeAreaView style={styles.container}><ScrollView style={styles.scroll}>{contactArray}</ScrollView></SafeAreaView>
+  return <View style={styles.dick}><SafeAreaView  style={styles.container}><ScrollView style={styles.scroll}>{contactArray}</ScrollView></SafeAreaView></View>
   function unStar(id, cinfo) {
-
     try {
-
-    cinfo.starredContacts.starredContacts.splice(starredContactList.indexOf(id), 1)
-      
+      cinfo.starredContacts.starredContacts.splice(starredContactList.indexOf(id), 1)  
       cinfo.saveStarredContacts()
       cinfo.loadStarredContacts()
       updateContactArray([])
       updateStarredContactList(cinfo.starredContacts.starredContacts)
-
     }
     catch (error) { console.error(error); }
   }
 }
 
 const styles = StyleSheet.create({
+  dick: {
+    backgroundColor: "#",
+    flex: 1,
+  },
   container: {
     justifyContent: 'space-evenly',
-    backgroundColor: "white",
   },
-  scroll: {},
+  scroll: {
+  },
   cardBox: {
     justifyContent: 'space-evenly',
     flex: 1,
@@ -64,9 +69,22 @@ const styles = StyleSheet.create({
     marginTop: 4,
     borderColor: '#C0C0C0',
     alignItems: 'center',
-    backgroundColor: "lightgray",
+    borderRadius: 200,
+    color:"red"
   },
   avatar: {},
   text: { fontSize: 20 },
-  icon: { fontSize: 50, borderColor: "black", borderWidth: 1 }
+  icon: { fontSize: 50, borderColor: "black", borderWidth: 1 },
+  luotsi: {
+    backgroundColor: '#18752b',
+  },
+  taitamo: {
+    backgroundColor: '#02607b',
+  },
+  topakka: {
+    backgroundColor: '#bd2805',
+  },
+  ohjaamo: {
+    backgroundColor: '#02295a',
+  },
 });
